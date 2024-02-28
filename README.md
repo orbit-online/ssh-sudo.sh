87fa1ca20382869129f6d90a749b5593082780d4
Anders Ingemann <aim@orbit.online>
2024-02-28 14:21:26 +0100

    Initial import

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # ssh-sudo
 
-Bash library for executing remote sudo commands with a cached password.
+Bash library for executing remote sudo commands while preserving the tty.
 
 ## Installation
 
@@ -10,22 +10,20 @@ With [μpkg](https://github.com/orbit-online/upkg)
 upkg install -g orbit-online/ssh-sudo.sh@<VERSION>
 ```
 
-Requires [socket-credential-cache](https://github.com/orbit-online/socket-credential-cache)
-to be set up.
-
 ## Functions
-
-### `ssh_cmd CMD...`
-
-Run command on the remote
 
 ### `ssh_sudo CMD...`
 
-Run command with `sudo` on the remote
+Run a command as root on the remote.
 
-### `get_sudo_pass`
+### `ssh_cmd CMD...`
 
-Get or prompt the `sudo` password.
+Run a command as `$SSH_USER` on the remote.
+
+### `ssh_sudo_cmd CMD...`
+
+Run a command as `$SSH_USER` on the remote but do not preserve the tty stdin
+(quicker).
 
 ## Environment variables
 
@@ -40,16 +38,3 @@ Remote SSH host _required_
 ### `$SSH_OPTS`
 
 Options to pass to all ssh invocations _optional_
-
-### `$SSH_STRIP_CR`
-
-Whether to strip carriage returns from the output _optional_.  
-Default: `true`
-
-### `$SSH_SUDO_PASS`
-
-`sudo` password to use (instead of prompting or retrieving it from cache) _optional_
-
-### `$SSH_SUDO_PASS_CACHE_TIMEOUT`
-
-Timeout for to set on `socket-credential-cache`. Defaults to the timeout that socket-credential-cache uses.
--- ssh-sudo.sh
+++ ssh-sudo.sh
@@ -1,36 +1,24 @@
 #!/usr/bin/env bash
 
-"$(dirname "$(realpath "${BASH_SOURCE[0]}")")/.upkg/.bin/checkdeps" socket-credential-cache
-
 ssh_cmd() {
-  local ssh_opts=(-t)
-  [[ -z $SSH_OPTS ]] || ssh_opts+=("${SSH_OPTS[@]}")
-  if ${SSH_STRIP_CR:-true}; then
-    ssh "${ssh_opts[@]}" "${SSH_USER:-$USER}@${SSH_HOST:?}" -- "$@" 2> >(sed 's/\r$//g' >&2) | sed 's/\r$//g'
-  else
-    ssh "${ssh_opts[@]}" "${SSH_USER:-$USER}@${SSH_HOST:?}" -- "$@"
-  fi
+  ssh "${SSH_OPTS[@]}" "${SSH_USER:-$USER}@${SSH_HOST:?}" -- "$@"
+}
+
+ssh_sudo_cmd() {
+  SSH_SUDO_PASS=$SSH_SUDO_PASS ssh_cmd sudo -Ski --prompt= "$@" <<<"${SSH_SUDO_PASS}"
 }
 
 ssh_sudo() {
-  ssh_cmd sudo -Ski --prompt= "$@" <<<"${SSH_SUDO_PASS:-$(get_sudo_pass "${SSH_USER:-$USER}" "${SSH_HOST:?}")}"
-}
-
-get_sudo_pass() {
-  local ssh_opts=(-t) cache_name="${SSH_USER:-$USER}@${SSH_HOST:?} sudo" sudo_pass
-  [[ ${#SSH_OPTS[@]} -eq 0 ]] || ssh_opts+=("${SSH_OPTS[@]}")
-  if ! sudo_pass=$(socket-credential-cache get "$cache_name" 2>/dev/null); then
-    printf "[sudo] password for %s on $SSH_HOST: " "${SSH_USER:-$USER}" >&2
-    read -rs sudo_pass || return 1
-    printf "\n" >&2
-    if [[ $(ssh "${ssh_opts[@]}" "${SSH_USER:-$USER}@$SSH_HOST" -- sudo -Sp \'\' echo success <<<"$sudo_pass") != 'success' ]]; then
-      return 1
-    fi
-    if [[ -n $SSH_SUDO_PASS_CACHE_TIMEOUT ]]; then
-      socket-credential-cache set -t "$SSH_SUDO_PASS_CACHE_TIMEOUT" "$cache_name" <<<"$sudo_pass"
-    else
-      socket-credential-cache set "$cache_name" <<<"$sudo_pass"
-    fi
-  fi
-  printf -- "%s\n" "$sudo_pass"
+  : "${SSH_SUDO_PASS:?"\$SSH_SUDO_PASS must be set when using ssh_sudo_script()"}"
+  local cmdpath askpath ret=0
+  cmdpath=$(ssh_sudo_cmd mktemp | sed 's/\r$//g')
+  askpath=$(ssh_cmd mktemp | sed 's/\r$//g')
+  ssh_sudo_cmd chmod u+x "$cmdpath" "$askpath"
+  ssh_cmd sudo -Ski --prompt= tee "$cmdpath" <<<"$SSH_SUDO_PASS
+$*" >/dev/null
+  ssh_cmd tee "$askpath" <<<"#!/usr/bin/env sh
+echo $SSH_SUDO_PASS" >/dev/null
+  ssh_cmd SUDO_ASKPASS="$askpath" sudo -A "$cmdpath" || ret=$?
+  ssh_sudo_cmd rm "$cmdpath" "$askpath"
+  return $ret
 }
