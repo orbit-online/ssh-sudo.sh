4b797b944e7ee8ae5ae8aa21bb9e13058c008af2
Anders Ingemann <aim@orbit.online>
2024-02-29 09:09:10 +0100

    Minor fixes, handle errexit not being set

--- ssh-sudo.sh
+++ ssh-sudo.sh
@@ -5,20 +5,19 @@ ssh_cmd() {
 }
 
 ssh_sudo_cmd() {
-  SSH_SUDO_PASS=$SSH_SUDO_PASS ssh_cmd sudo -Ski --prompt= "$@" <<<"${SSH_SUDO_PASS}"
+  ssh_cmd sudo -Ski --prompt= "$@" <<<"${SSH_SUDO_PASS:?}"
 }
 
 ssh_sudo() {
-  : "${SSH_SUDO_PASS:?"\$SSH_SUDO_PASS must be set when using ssh_sudo_script()"}"
   local cmdpath askpath ret=0
-  cmdpath=$(ssh_sudo_cmd mktemp | sed 's/\r$//g')
-  askpath=$(ssh_cmd mktemp | sed 's/\r$//g')
-  ssh_sudo_cmd chmod u+x "$cmdpath" "$askpath"
+  cmdpath=$(ssh_sudo_cmd mktemp | sed 's/\r$//g') || return $?
+  askpath=$(ssh_cmd mktemp | sed 's/\r$//g') || return $?
+  ssh_sudo_cmd chmod u+x "$cmdpath" "$askpath" || return $?
   ssh_cmd sudo -Ski --prompt= tee "$cmdpath" <<<"$SSH_SUDO_PASS
-$*" >/dev/null
+$*" >/dev/null || return $?
   ssh_cmd tee "$askpath" <<<"#!/usr/bin/env sh
-echo $SSH_SUDO_PASS" >/dev/null
+echo $SSH_SUDO_PASS" >/dev/null || return $?
   ssh_cmd SUDO_ASKPASS="$askpath" sudo -A "$cmdpath" || ret=$?
-  ssh_sudo_cmd rm "$cmdpath" "$askpath"
+  ssh_sudo_cmd rm "$cmdpath" "$askpath" || return $?
   return $ret
 }
