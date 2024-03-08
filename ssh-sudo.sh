821be38f9ff74a41f2d36581210fd85910470a32
Anders Ingemann <aim@orbit.online>
2024-03-08 09:52:33 +0100

    Rename cmdpath to scriptpath

--- ssh-sudo.sh
+++ ssh-sudo.sh
@@ -9,17 +9,17 @@ ssh_sudo_cmd() {
 }
 
 ssh_sudo() {
-  local cmdpath askpath ret=0
-  cmdpath=$(ssh_sudo_cmd mktemp | sed 's/\r$//g') || return $?
-  ssh_sudo_cmd chmod u+x "$cmdpath" || return $?
+  local scriptpath askpath ret=0
+  scriptpath=$(ssh_sudo_cmd mktemp | sed 's/\r$//g') || return $?
+  ssh_sudo_cmd chmod u+x "$scriptpath" || return $?
   askpath=$(ssh_cmd mktemp | sed 's/\r$//g') || return $?
   ssh_cmd chmod u+x "$askpath" || return $?
-  ssh_cmd sudo -Sk --prompt= tee "$cmdpath" <<<"$SSH_SUDO_PASS
+  ssh_cmd sudo -Sk --prompt= tee "$scriptpath" <<<"$SSH_SUDO_PASS
 $*" >/dev/null || return $?
   ssh_cmd tee "$askpath" <<<"#!/usr/bin/env sh
 echo $SSH_SUDO_PASS" >/dev/null || return $?
-  ssh_cmd SUDO_ASKPASS="$askpath" sudo -A "$cmdpath" || ret=$?
+  ssh_cmd SUDO_ASKPASS="$askpath" sudo -A "$scriptpath" || ret=$?
   ssh_cmd rm "$askpath" || true
-  ssh_sudo_cmd rm "$cmdpath" || true
+  ssh_sudo_cmd rm "$scriptpath" || true
   return $ret
 }
