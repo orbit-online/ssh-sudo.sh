08db0d511eab7e8a9cae9aa5239e3e96e4e5573b
Anders Ingemann <aim@orbit.online>
2024-03-08 09:51:50 +0100

    Split up setting executable flags on askpath & cmdpath

--- ssh-sudo.sh
+++ ssh-sudo.sh
@@ -11,13 +11,15 @@ ssh_sudo_cmd() {
 ssh_sudo() {
   local cmdpath askpath ret=0
   cmdpath=$(ssh_sudo_cmd mktemp | sed 's/\r$//g') || return $?
+  ssh_sudo_cmd chmod u+x "$cmdpath" || return $?
   askpath=$(ssh_cmd mktemp | sed 's/\r$//g') || return $?
-  ssh_sudo_cmd chmod u+x "$cmdpath" "$askpath" || return $?
+  ssh_cmd chmod u+x "$askpath" || return $?
   ssh_cmd sudo -Sk --prompt= tee "$cmdpath" <<<"$SSH_SUDO_PASS
 $*" >/dev/null || return $?
   ssh_cmd tee "$askpath" <<<"#!/usr/bin/env sh
 echo $SSH_SUDO_PASS" >/dev/null || return $?
   ssh_cmd SUDO_ASKPASS="$askpath" sudo -A "$cmdpath" || ret=$?
-  ssh_sudo_cmd rm "$cmdpath" "$askpath" || return $?
+  ssh_cmd rm "$askpath" || true
+  ssh_sudo_cmd rm "$cmdpath" || true
   return $ret
 }
